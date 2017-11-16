761a920d8ee4fd4f07616cb0e4dfbc0042762f15
yaoshun <shun.yao@mobvista.com>
2017-11-16 16:49:24 +0800

    add demo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var lessMiddleware = require('less-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var demo = require('./routes/demo');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(lessMiddleware(path.join(__dirname, 'less'), {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
+app.use('/demo', demo)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
